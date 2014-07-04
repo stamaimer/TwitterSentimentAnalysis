fd305a13d3563281355a7ccc9078ec52c1de5fe4
stamaimer <stamaimer@gmail.com>
2014-07-04 20:55:02 +0800

    step by step

--- tfidf.py
+++ tfidf.py
@@ -1,8 +1,31 @@
+import os
 import nltk
+from __future__ import division
+
+def prepare(path2corpus):
+	
+	files = os.listdir(path2corpus)
+
+	filelists = {}
+
+	for file in files:
+
+		filelists[files] = [term for term in open(file, 'r').read(-1).split()]
+
+	return filelists
 
 def cal_term_freq(text):
 
-	return nltk.FreqDist([term for term in text.split()])
+	terms    = [term for term in text.split()]
+
+	length   = len(terms)
+
+	freqdist = nltk.FreqDist(terms)
+
+	for key in freqdist.keys():
+
+		freqdist[key] = freqdist[key] / length
+
+	return freqdist
 
 def cal_idoc_freq(dist):
-	pass
