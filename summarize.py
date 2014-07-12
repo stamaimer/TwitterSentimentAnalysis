477c628ed696d7c061a574352cc93391309359c4
stamaimer <stamaimer@gmail.com>
2014-07-12 17:44:24 +0800

    take a break

--- summarize.py
+++ summarize.py
@@ -4,11 +4,44 @@ import nltk
 import numpy
 import tfidf
 
-#
-CLUSTER_THRESHOLD = 5 #distance between words to consider
-#
+N = 100 				# number of words yto consider
+CLUSTER_THRESHOLD = 5 	# distance between words to considers
 
-def summarize(text):
+# approach taken from "The Automatic Creation of Literature Abstracts" by H.P. Luhn
+
+def placeholder(sentences, keywords):
+
+	scores = []
+
+	sent_idx = -1
+
+	for sentence in [nltk.tokenize.word_tokenize(sentence) for sentence in sentences]:
+
+		sent_idx += 1
+
+		word_idx = []
+
+		for word in keywords:
+
+			try:
+
+				word_idx.append(sentence.index(word))
+
+			except ValueError, e:
+				
+				pass
+
+		word_idx.sort()
+		
+		if 0 == len(word_idx): continue
+
+		 
+
+def summarize(text, path2corpus):
 	
-	keywords = tfidf.gen_keywords(text)
+	keywords  = tfidf.gen_keywords(text, path2corpus, N)
+
+	sentences = [sentence.lower() for sentence in nltk.tokenize.sent_tokenize(text)]
+
+	sentences = placeholder(sentences, keywords)
 
