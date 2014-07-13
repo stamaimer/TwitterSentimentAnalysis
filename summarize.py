75a24ef3d733423339ff4d568fd6a8fb5a580943
stamaimer <stamaimer@gmail.com>
2014-07-13 18:13:53 +0800

    almost finishe

--- summarize.py
+++ summarize.py
@@ -31,11 +31,50 @@ def placeholder(sentences, keywords):
 				
 				pass
 
-		word_idx.sort()
-		
 		if 0 == len(word_idx): continue
 
-		 
+		word_idx.sort()
+		
+		clusters = []
+
+		cluster  = [word_idx[0]]
+
+		i = 1
+
+		while i < len(word_idx):
+		 	
+		 	if word_idx[i] - word_idx[i - 1] < CLUSTER_THRESHOLD:
+
+		 		cluster.append(word_idx[i])
+
+		 	else:
+
+		 		clusters.append(cluster[:])
+
+		 		cluster = [word_idx[i]]
+
+		 	i += 1
+
+		clusters.append(cluster)
+
+		max_cluster_score = 0
+
+		for cluster in clusters:
+
+			count_of_keywords = len(cluster)
+
+			cluster_length = cluster[-1] - cluster[0] + 1
+
+			score = 1.0 * count_of_keywords * count_of_keywords / cluster_length
+
+			if score > max_cluster_score:
+
+				max_cluster_score = score
+
+		scores.append((sent_idx, max_cluster_score))
+
+	return scores
+
 
 def summarize(text, path2corpus):
 	
@@ -44,4 +83,6 @@ def summarize(text, path2corpus):
 	sentences = [sentence.lower() for sentence in nltk.tokenize.sent_tokenize(text)]
 
 	sentences = placeholder(sentences, keywords)
+
+	
 
