78261f7659e4ce62a7a4bac3cbe542fd03a4483b
stamaimer <stamaimer@gmail.com>
2014-10-14 08:46:44 -0400

    refactor

--- preprocess.py
+++ preprocess.py
@@ -1,115 +1,27 @@
-import re
-import sys
+import util
 import nltk
-import tfidf
-import codecs
-import utilities
-import preprocessim
-import twitter_text
-import mysql.connector
-from twitter_text import TwitterText
 from nltk.stem.wordnet import WordNetLemmatizer
 
-stopwords = nltk.corpus.stopwords.words('english') + ['mh17', 'says', 'url', 'username', u'\u2026', '\'', '&', 'via', 'must', 'may', 'it\'s', 'u', '-', 'another', 'please', 'say', 'many', '9', '6', '4', '3', '2', '1', '+', 'goes', 'i\'m', ':', '.', ',', '17', 'prayformh', 'news', 'gaza', 'mh370', 'people', 'black', 'us', 'world', 'flight', 'airlines', 'passengers', 'plane', 'families', 'aids', 'lost', 'crew', 'one', 'train', 'air', '']	#
+stopwords = nltk.corpus.stopwords.words('english') + ['']
 
-tokenizer = utilities.Tokenizer()
+tokenizer = util.Tokenizer()
 
-def connectsql():
-	
-	connection = mysql.connector.connect(user = 'root',
-										 password = '',
-										 host = '127.0.0.1',
-										 database = 'twitter')
+def del_stopwords(tweet):
 
-	return connection
+    for term in tweet:
 
-def pre_process(text):
-	
-	extractor = TwitterText(text).extractor
+        if term in stopwords:
 
-	for ele in extractor.extract_urls_with_indices():
+            tweet.remove(term)
 
-		text = re.sub(ele['url'], '', text)
+    return tweet
 
-	text += ' '
+def lemmatize(tweet):
 
-	text = re.sub('RT', ' ', text)
+    lmtzr = WordNetLemmatizer()
 
-	text = re.sub('&amp;', ' ', text)
+    for i in range(len(tweet)):
 
-	text = re.sub('@.*?[: ]', ' ', text)
+        tweet[i] = lmtzr.lemmatize(tweet[i])
 
-	text = re.sub('[][#|.,!/?*_+"]', ' ', text)
-
-	return text
-
-def del_stopwords(terms):
-	
-	for term in terms:
-
-		if term in stopwords:
-
-			terms.remove(term)
-
-	return terms
-
-def lemmatize(terms):
-
-	lmtzr = WordNetLemmatizer()
-	
-	for i in range(len(terms)):
-
-		terms[i] = lmtzr.lemmatize(terms[i])
-
-	return terms
-
-connection = connectsql()
-
-cursor = connection.cursor()
-
-sql = "SELECT tweet_id, tweet_text FROM tweets LIMIT 0, 2500"
-
-cursor.execute(sql)
-
-results = cursor.fetchall()
-
-for(id, text) in results:
-
-	text = preprocessim.preprocess(text)
-
-	print text
-
-	terms = [term for term in tokenizer.tokenize(text)]
-
-	print terms
-
-	# bigrams = nltk.bigrams(text.split())
-
-	terms = del_stopwords(terms)
-
-	print terms
-
-	terms = lemmatize(terms)
-
-	print terms
-
-	keywords  = tfidf.gen_keywords(' '.join(terms), './corpus', 10)
-
-	for i in range(len(keywords)):
-
-		keywords[i] = keywords[i][0][0]
-
-	print ' '.join(keywords)
-
-	sql = "UPDATE tweets SET tweet_pre_process_result = \"%s\" WHERE tweet_id = %d" % (' '.join(keywords), id)
-
-	cursor.execute(sql)
-
-	connection.commit()
-
-
-
-
-
-	
-
+    return tweet
