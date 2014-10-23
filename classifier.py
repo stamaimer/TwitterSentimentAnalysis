797069f3912ba7d3c04b6a3a7bfc61540ce59058
stamaimer <stamaimer@gmail.com>
2014-10-23 11:16:19 -0400

    add gen_featvect

--- classifier.py
+++ classifier.py
@@ -1,5 +1,8 @@
 import nltk
+import util
 import collections
+
+deal = [1, util.Tokenizer().tokenize, nltk.bigrams, nltk.trigrams]
 
 def get_ngrams(twitter, n):
 
@@ -27,14 +30,74 @@ def get_ngrams(twitter, n):
 
 def gen_featvects(twitter, n):
 
+    collocations = get_ngrams(twitter, n)
+
     labeled = twitter.labeled
 
     count = labeled.count()
 
     labeled = labeled.find({}, {'text':1, 'label':1, '_id':0})
 
+    featvect = [None for _ in range(count)]
+
     for item in labeled:
 
+        featdist = {}
 
+        if 1 == n:
+
+        results = deal[n](item['text'])
+
+        else:
+
+        results = deal[n](deal[1](item['text']))
+
+        for collocation in collocations:
+
+            if collocation in results:
+
+                featdist[collocation] = 1
+
+            else:
+
+                featdist[collocation] = 0
+
+        featvect.append((featdist, item[label]))
+
+    return featvect
 
 def naive_bayes_classifier(twitter):
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
