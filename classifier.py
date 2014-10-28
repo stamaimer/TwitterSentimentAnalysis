b60739aecf157ea4303d742351d958095985b5a6
stamaimer <stamaimer@gmail.com>
2014-10-27 22:25:43 -0400

    remove emoticons in unigram

--- classifier.py
+++ classifier.py
@@ -2,6 +2,24 @@ import nltk
 import util
 import collections
 
+icons = [u'\U0001f600', u'\U0001f601', u'\U0001f602', u'\U0001f604',
+         u'\U0001f606', u'\u0001f607', u'\U0001f608', u'\U0001f60A',
+         u'\U0001F60B', u'\U0001f60c', u'\U0001f60E', u'\U0001f60f',
+         u'\U0001f61b', u'\U0001f61c', u'\U0001f61D', u'\U0001f62c',
+         u'\U0001f638', u'\U0001f639', u'\U0001f63A', u'\U0001f63C',
+         u'\U0001f642', u'\U0001f648', u'\U0001f649', u'\U0001f64B',
+         u'\U0001f617', u'\U0001f618', u'\U0001f619', u'\U0001f61a',
+         u'\U0001f63b', u'\U0001f63d', u'\U0001f646', u'\U0001f647',
+         u'\U0001f64f', u'\U0001f613', u'\U0001f61f', u'\U0001f628',
+         u'\U0001f630', u'\U0001f631', u'\U0001f64a', u'\U0001f605',
+         u'\U0001f62e', u'\U0001f62f', u'\U0001f632', u'\U0001f633',
+         u'\U0001f640', u'\U0001f614', u'\U0001f61e', u'\U0001f622',
+         u'\U0001f623', u'\U0001f625', u'\U0001f626', u'\U0001f627',
+         u'\U0001f629', u'\U0001f62b', u'\U0001f62d', u'\U0001f63f',
+         u'\U0001f641', u'\U0001f64d', u'\U0001f64e', u'\U0001f610',
+         u'\U0001f612', u'\U0001f615', u'\U0001f620', u'\U0001f621',
+         u'\U0001f609']
+
 deal = [1, util.Tokenizer().tokenize, nltk.bigrams, nltk.trigrams]
 
 def get_ngrams(twitter, n):
@@ -34,7 +52,9 @@ def get_ngrams(twitter, n):
 
     for record in records:
 
-        collocations.append(record['collocation'])
+        if record['collocation'] not in icons:
+
+            collocations.append(record['collocation'])
 
     return collocations
 
