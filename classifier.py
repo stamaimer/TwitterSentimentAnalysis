1d4f844a3528ee8eba061ae94ca863ebf40a61ee
stamaimer <stamaimer@gmail.com>
2014-10-28 22:42:27 +0800

    decrease the number of train set

--- classifier.py
+++ classifier.py
@@ -2,23 +2,26 @@ import nltk
 import util
 import collections
 
-icons = [u'\U0001f600', u'\U0001f601', u'\U0001f602', u'\U0001f604',
-         u'\U0001f606', u'\u0001f607', u'\U0001f608', u'\U0001f60A',
-         u'\U0001F60B', u'\U0001f60c', u'\U0001f60E', u'\U0001f60f',
-         u'\U0001f61b', u'\U0001f61c', u'\U0001f61D', u'\U0001f62c',
-         u'\U0001f638', u'\U0001f639', u'\U0001f63A', u'\U0001f63C',
-         u'\U0001f642', u'\U0001f648', u'\U0001f649', u'\U0001f64B',
-         u'\U0001f617', u'\U0001f618', u'\U0001f619', u'\U0001f61a',
-         u'\U0001f63b', u'\U0001f63d', u'\U0001f646', u'\U0001f647',
-         u'\U0001f64f', u'\U0001f613', u'\U0001f61f', u'\U0001f628',
-         u'\U0001f630', u'\U0001f631', u'\U0001f64a', u'\U0001f605',
-         u'\U0001f62e', u'\U0001f62f', u'\U0001f632', u'\U0001f633',
-         u'\U0001f640', u'\U0001f614', u'\U0001f61e', u'\U0001f622',
+icons = [u'\U0001f600', u'\U0001f601', u'\U0001f602', u'\U0001f603',
+         u'\U0001f604', u'\U0001f606', u'\u0001f607', u'\U0001f608',
+         u'\U0001f60A', u'\U0001F60B', u'\U0001f60c', u'\U0001f60E',
+         u'\U0001f60f', u'\U0001f61b', u'\U0001f61c', u'\U0001f61D',
+         u'\U0001f62c', u'\U0001f638', u'\U0001f639', u'\U0001f63A',
+         u'\U0001f63C', u'\U0001f642', u'\U0001f648', u'\U0001f649',
+         u'\U0001f64B', u'\U0001f60D', u'\U0001f617', u'\U0001f618',
+         u'\U0001f619', u'\U0001f61a', u'\U0001f63b', u'\U0001f63d',
+         u'\U0001f646', u'\U0001f647', u'\U0001f64f', u'\U0001f613',
+         u'\U0001f61f', u'\U0001f628', u'\U0001f630', u'\U0001f631',
+         u'\U0001f64a', u'\U0001f605', u'\U0001f62e', u'\U0001f62f',
+         u'\U0001f632', u'\U0001f633', u'\U00016f35', u'\U0001f640',
+         u'\U0001f614', u'\U0001f616', u'\U0001f61e', u'\U0001f622',
          u'\U0001f623', u'\U0001f625', u'\U0001f626', u'\U0001f627',
          u'\U0001f629', u'\U0001f62b', u'\U0001f62d', u'\U0001f63f',
          u'\U0001f641', u'\U0001f64d', u'\U0001f64e', u'\U0001f610',
-         u'\U0001f612', u'\U0001f615', u'\U0001f620', u'\U0001f621',
+         u'\U0001f611', u'\U0001f612', u'\U0001f615', u'\U0001f645',
+         u'\U0001f620', u'\U0001f621', u'\U0001f624', u'\U0001f63e', 
          u'\U0001f609']
+
 
 deal = [1, util.Tokenizer().tokenize, nltk.bigrams, nltk.trigrams]
 
@@ -36,7 +39,7 @@ def get_ngrams(twitter, n):
 
         collection = twitter.bigram
 
-        records = collection.find({'frequency':{'$gte':1460, '$lte':107699}}, {'collocation':1, '_id':0})
+        records = collection.find({'frequency':{'$gte':75, '$lte':98}}, {'collocation':1, '_id':0})
 
         print 'generate bigram'
 
@@ -44,7 +47,7 @@ def get_ngrams(twitter, n):
 
         collection = twitter.trigram
 
-        records = collection.find({'frequency':{'$gte':1460, '$lte':107699}}, {'collocation':1, '_id':0})
+        records = collection.find({'frequency':{'$gte':2, '$lte':2}}, {'collocation':1, '_id':0})
 
         print 'generate trigram'
 
@@ -66,7 +69,7 @@ def gen_featvect(twitter, n):
 
     count = labeled.count()
 
-    labeled = labeled.find({}, {'text':1, 'label':1, '_id':0})
+    labeled = labeled.find({}, {'text':1, 'label':1, '_id':0}).limit(100000)
 
     featvect = []
 
@@ -106,9 +109,9 @@ def naive_bayes_classifier(twitter, n):
 
     for i in range(5):
 
-        testset = featvect[68577 * i : 68577 * (i + 1)]
+        testset = featvect[20000 * i : 20000 * (i + 1)]
 
-        del featvect[68577 * i : 68577 * (i + 1)]
+        del featvect[20000 * i : 20000 * (i + 1)]
 
         trainset = featvect
 
